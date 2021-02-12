0e131cd9c3a3b4dad7e441af1bf61c6a13f68a0e
ZYCGary <zyc.gary@gmail.com>
2021-02-12 12:47:24 +1100

    add configurable variables

--- user_upload.php
+++ user_upload.php
@@ -1,1 +1,24 @@
 <?php
+
+use Dotenv\Dotenv as Dotenv;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+function OpenCon()
+{
+    $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE'])
+    or die("Connect failed: %s\n" . $conn->error);
+
+    return $conn;
+}
+
+function CloseCon($conn)
+{
+    $conn->close();
+}
+
+OpenCon();
+
